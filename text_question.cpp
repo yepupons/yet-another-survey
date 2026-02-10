ef358693ec32794186000afd4ce3ade5d957f4d3
Krivnik <nkrivitcenko@gmail.com>
2026-02-10 13:08:53 +0300

    text-block realization

---
+++ text_question.cpp
@@ -0,0 +1,22 @@
+#include "text_question.hpp"
+#include <iostream>
+#include <nlohmann/json.hpp>
+
+namespace survey {
+void TextBlock::print() const {
+    std::cout << question_ << '\n';
+    std::cout << "Input your answer here: ";
+}
+
+bool TextBlock::parse_input(const std::string &input) {
+    if (!input.empty()) {
+        answer_ = input;
+        return true;
+    }
+    return false;
+}
+
+void TextBlock::save_result(nlohmann::json& answers_data) const {
+    answers_data["answers"].push_back(answer_);
+}
+}
